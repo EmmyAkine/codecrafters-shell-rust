7441fe45a1782963a1c3d6e8f82751664a8b1943
Emmy <emmanueloyebamiji03@gmail.com>
2026-08-05 12:14:49 +0100

    codecrafters submit [skip ci]

--- src/builtins/echo.rs
+++ src/builtins/echo.rs
@@ -13,7 +13,7 @@ impl EchoCommand{
 impl Command for EchoCommand  {
     fn get_name_copy(&self) -> String{ self.name.clone() }
     fn execute(&self, args: &[&str]) -> bool {
-        println!("{}", args.join(" "));
+        print!("{}", args.join(" "));
         true
     }
 }
